9307078a3c78176a9961e8b44997941a7883fb41
iamnotrodger <rodger.retanal24@gmail.com>
2022-01-19 19:33:19 -0500

    [update] QueryClientProvider + Routes for App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-import { Fragment } from 'react';
+import { useEffect } from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { loadLikes } from './api/AstronomyAPI';
 import './App.css';
+import HomePage from './pages/HomePage';
+
+const queryClient = new QueryClient();
 
 const App = () => {
-	return <Fragment></Fragment>;
+	useEffect(() => {
+		loadLikes();
+	}, []);
+
+	return (
+		<QueryClientProvider client={queryClient}>
+			<BrowserRouter>
+				<Routes>
+					<Route path='/' element={<HomePage />} />
+				</Routes>
+			</BrowserRouter>
+		</QueryClientProvider>
+	);
 };
 
 export default App;
