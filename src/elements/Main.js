b22c00a0dfd43c20687ba1da1404f1635ff7b67e
iamnotrodger <rodger.retanal24@gmail.com>
2022-01-20 07:45:27 -0500

    [update] Main elmeent style on small screens size

--- src/elements/Main.js
+++ src/elements/Main.js
@@ -1,6 +1,11 @@
 import styled from 'styled-components';
+import breakpoints from '../utils/breakpoints';
 
 export default styled.main`
 	background-color: rgba(var(--b3f, 250, 250, 250), 1);
-	padding: 2rem;
+	padding: 0.5rem;
+
+	@media ${breakpoints.md} {
+		padding: 2rem;
+	}
 `;
