6f8d3c9dbc4be05bc6510382636309885744c103
iamnotrodger <rodger.retanal24@gmail.com>
2022-01-20 07:01:10 -0500

    [feature] Infnite Scroll to AstronomyList

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,38 +1,55 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { useInfiniteQuery } from 'react-query';
+import { ClipLoader } from 'react-spinners';
 import { getManyAstronomy } from '../api/AstronomyAPI';
 import AstronomyList from '../components/AstronomyList';
+import LoaderContainer from '../elements/LoaderContainer';
 import Main from '../elements/Main';
+import useIntersectionObserver from '../hooks/useIntersectionObserver';
 
 const TODAY = new Date();
 const MAX_LIMIT = process.env.REACT_APP_ASTRONOMY_LIMIT || 15;
 
 const HomePage = () => {
-	const fetchAstronomy = ({ pageParam = TODAY }) => {
+	const fetchAstronomy = ({ pageParam: endDate = TODAY }) => {
 		const startDate = new Date();
-		startDate.setDate(pageParam.getDate() - MAX_LIMIT);
-		return getManyAstronomy(startDate, pageParam);
+		const dateOffSet = 24 * 60 * 60 * 1000 * MAX_LIMIT;
+		startDate.setTime(endDate.getTime() - dateOffSet);
+		return getManyAstronomy(startDate, endDate);
 	};
 
-	const { data, isFetching } = useInfiniteQuery('astronomy', fetchAstronomy, {
-		getNextPageParam: (lastPage, pages) => {
-			const lastDate = lastPage[lastPage.length - 1].date;
+	const { data, isFetching, hasNextPage, fetchNextPage } = useInfiniteQuery(
+		'astronomy',
+		fetchAstronomy,
+		{
+			getNextPageParam: (lastPage, pages) => {
+				const lastDate = lastPage[lastPage.length - 1].date;
+				try {
+					if (lastDate) return new Date(lastDate);
+				} catch (error) {
+					console.log(error);
+				}
+				return undefined;
+			},
+			staleTime: 60 * 1000,
+		}
+	);
 
-			try {
-				if (lastDate) return new Date(lastDate);
-			} catch (error) {
-				console.log(error);
-			}
-
-			return undefined;
-		},
-		staleTime: 60 * 1000,
+	const endScrollRef = useRef();
+	useIntersectionObserver({
+		target: endScrollRef,
+		onIntersect: fetchNextPage,
+		enabled: hasNextPage,
+		rootMargin: '100px',
 	});
 
-	if (isFetching) return <div>loading</div>;
+	const astronomyList = data ? data.pages.flat() : [];
 	return (
 		<Main>
-			<AstronomyList items={data.pages.flat().reverse()} />
+			<AstronomyList items={astronomyList} />
+			<LoaderContainer ref={endScrollRef}>
+				<ClipLoader color='#000000' loading={isFetching} size={35} />
+			</LoaderContainer>
 		</Main>
 	);
 };
